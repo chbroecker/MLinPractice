ca169e969add76eeab1b020d7afe79384af55640
Christopher Bröcker <broecker.christopher@gmail.com>
2021-10-28 18:11:19 +0200

    fix file and method naming

--- test/preprocessing/dayperiodizer_test.py
+++ test/preprocessing/dayperiodizer_test.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  27 16:23:46 2021
+
+@author: chbroecker
+"""
 
 import unittest
 from src.preprocessing.dayperiodizer import DayPeriodizer
 
 
-class DaytimeCheckTest(unittest.TestCase):
-    def test_daytime_check(self):
+class DayperiodizerTest(unittest.TestCase):
+    def test_dayperiodizer(self):
         input_times = ['18:29:04', '07:46:31', '03:24:09', '00:29:44', '19:45:15']
         output_periods = ['Evening', 'Morning', 'Night', 'Night', 'Evening']
 
